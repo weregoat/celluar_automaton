01c2e0d58abc7fd3b317cf404e0e674928dd5d11
Ruggero Cucchiani <ruggero.cucchiani@exertisztorm.com>
2018-07-12 20:46:47 +0200

    add rule option

--- eca.go
+++ eca.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"flag"
 	"time"
+	"strconv"
+	"os"
 )
 
 type cell struct {
@@ -16,12 +18,34 @@ type cell struct {
 const alive = 'X'
 const dead = ' '
 
+
+/* Patterns in sequence */
+var patterns = []string{"111","110","101","100","011","010","001","000"}
+var rule int64
+
+/* Empty table with pattern as key and the value as state */
+var table = map[string]uint8 {
+	"111":0,
+	"110":0,
+	"101":0,
+	"100":0,
+	"011":0,
+	"010":0,
+	"001":0,
+	"000":0,
+}
+
 func main() {
 	iterations := flag.Int("iterations", 20, "number of iterations")
-	//rule := flag.Int64("rule", 110, "rule number")
+	flag.Int64Var(&rule,"rule", 110, "rule number")
 	cells := flag.Int("cells", 32, "number of cells")
 	flag.Parse()
-	cellLine := make([]int, *cells, *cells)
+	if rule < 0 || rule > 255 {
+		fmt.Println("Invalid rule number")
+		os.Exit(1)
+	}
+	table = populateTable(rule)
+	cellLine := make([]uint8, *cells, *cells)
 	i := 0
 	initialize(cellLine)
 	printLine(cellLine)
@@ -36,7 +60,7 @@ func main() {
 
 }
 
-func initialize(cells []int) {
+func initialize(cells []uint8) {
 	seed := rand.NewSource(time.Now().UnixNano())
 	randomiser := rand.New(seed)
 	for i := range cells {
@@ -49,14 +73,14 @@ func initialize(cells []int) {
 	}
 }
 
-func update(cells []int) {
+func update(cells []uint8) {
 
-	previous := make([]int, len(cells), cap(cells))
+	previous := make([]uint8, len(cells), cap(cells))
 	copy(previous, cells)
 	for i := range previous {
-		var left int
-		var center int
-		var right int
+		var left uint8
+		var center uint8
+		var right uint8
 		/* Special cases */
 		if i == 0 {
 			left = previous[len(previous)-1]
@@ -69,21 +93,13 @@ func update(cells []int) {
 			right = previous[i+1]
 		}
 		center = previous[i]
-		cells[i] = applyRule(left, center, right)
+		pattern := fmt.Sprintf("%d%d%d", left,center,right)
+		cells[i] = table[pattern]
 	}
 }
 
-func applyRule(left, center, right int) int{
-	state := 1
-	pattern := fmt.Sprintf("%d%d%d", left,center,right)
-	switch pattern {
-	case "111", "100", "000":
-		state = 0
-	}
-	return state
-}
 
-func printLine(cells []int) {
+func printLine(cells []uint8) {
 	for _,value := range cells {
 		symbol := dead
 		if value == 1 {
@@ -94,32 +110,19 @@ func printLine(cells []int) {
 	fmt.Print("\n")
 }
 
-/*
-func generateTable(rule int64) map[string]rune{
-	/* Initialize the transformation table */
-	/* The key is the LCR cell combination */
-	/* The value is the center next value */
-	/*
-	table := map[string]rune {
-		"111": dead,
-		"110": dead,
-		"101": dead,
-		"100": dead,
-		"011": dead,
-		"010": dead,
-		"001": dead,
-		"000": dead,
+
+func populateTable(rule int64) map[string]uint8 {
+
+	/* Generate the rule as a binary string */
+	binaryString := fmt.Sprintf("%08s",strconv.FormatInt(rule,2))
+	for i := 0; i < len(binaryString); i++ {
+		value := rune(binaryString[i])
+		if value == '1' {
+			table[patterns[i]] = 1
+		}
 	}
-	binaryString := strconv.FormatInt(rule,2)
-	for i := len(binaryString); i >= 0; i-- {
-		value := binaryString[i]
-		
-	}
-	fmt.Println(string(binaryString[0]))
 	return table
-
-
 }
-*/
+
 
 
